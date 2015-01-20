177e4cc199d7a0f0b96ece275ec56847a6b2f5c1
Howy Perrin <howard@perrin.co.uk>
2015-01-20 14:01:10 +0000

    Demo scheduler via auctions ending on time

--- build.sbt
+++ build.sbt
@@ -5,9 +5,10 @@ version := "1.0"
 scalaVersion := "2.11.5"
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor" % "2.3.9",
-  "com.typesafe.akka" %% "akka-testkit" % "2.3.9" % "test",
-  "org.specs2" %% "specs2-core" % "2.4.15" % "test"
+  "com.typesafe.akka" %% "akka-actor"   % "2.3.9",
+  "joda-time"         %  "joda-time"    % "2.2",
+  "com.typesafe.akka" %% "akka-testkit" % "2.3.9"  % "test",
+  "org.specs2"        %% "specs2-core"  % "2.4.15" % "test"
 )
 
 scalacOptions in Test ++= Seq("-Yrangepos")
--- src/main/scala/Auction.scala
+++ src/main/scala/Auction.scala
@@ -1,20 +1,35 @@
 import akka.actor.Actor
+import org.joda.time.{Interval, DateTime}
+import scala.concurrent.duration.FiniteDuration
+import java.util.concurrent.TimeUnit
+import DateTime.now
 
-class Auction extends Actor {
+class Auction(endTime: DateTime) extends Actor {
   import Auction.Protocol._
 
   var currentHighestBid = BigDecimal(0)
+  var currentStatus: State = Running
+
+  context.system.scheduler.scheduleOnce(atEndTime(), self, EndNotification)(context.system.dispatcher)
 
   def receive = {
-    case StatusRequest => sender ! StatusResponse(currentHighestBid)
-    case Bid(value)    => currentHighestBid = currentHighestBid max value
+    case StatusRequest   => sender ! StatusResponse(currentHighestBid, currentStatus)
+    case Bid(value)      => currentHighestBid = currentHighestBid max value
+    case EndNotification => currentStatus = Ended
   }
+
+  def atEndTime() = FiniteDuration(new Interval(now, endTime).toDurationMillis, TimeUnit.MILLISECONDS)
 }
+
 object Auction {
   object Protocol {
     case object StatusRequest
-    case class StatusResponse(currentHighestBid: BigDecimal)
+    case class StatusResponse(currentHighestBid: BigDecimal, state: State)
     case class Bid(value: BigDecimal)
+    case object EndNotification
+
+    sealed trait State
+    case object Running extends State
+    case object Ended extends State
   }
 }
-
